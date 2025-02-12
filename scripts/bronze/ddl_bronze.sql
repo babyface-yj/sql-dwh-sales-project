b747fabb40354417c0c67fd380863888a8f00b7c
David Huang <huangyongjian5@gmail.com>
2025-02-12 17:14:11 +0800

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -30,7 +30,7 @@ IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
     DROP TABLE bronze.crm_prd_info;
 CREATE TABLE bronze.crm_prd_info(
     prd_id INT,
-    prd_info NVARCHAR(50),
+    prd_key NVARCHAR(50),
     prd_nm NVARCHAR(50),
     prd_cost INT,
     prd_line NVARCHAR(50),
